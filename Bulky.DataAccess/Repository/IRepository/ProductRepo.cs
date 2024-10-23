f901339c025e50ea2cedb35e1c5b4a69f2e22fdc
Mahmoud0sama <MahmoudOsamaSaad21@gmail.com>
2024-10-23 13:29:29 +0300

    Product CRUD Added

---
+++ Bulky.DataAccess/Repository/IRepository/ProductRepo.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.Models;
+using BulkyWeb.DataAccess.Data;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public class ProductRepo : Repository<Product>, IProductRepo
+    {
+        private readonly AppDbContext _db;
+
+        public ProductRepo(AppDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+        public void Update(Product obj)
+        {
+            _db.Update(obj);
+        }
+
+
+    }
+}
